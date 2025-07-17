1927a909c4bf30f1a4987220fb2962c211bdeb34
Mahesh babu Cherukuri <maheshbabucherukuri1997@gmail.com>
2025-07-17 17:07:30 +0530

    add JPA annotations to Auditorium and BaseModel; create project configuration files

--- Low-level-design-folder/bookmyshow/src/main/java/com/lld/bookmyshow/models/Auditorium.java
+++ Low-level-design-folder/bookmyshow/src/main/java/com/lld/bookmyshow/models/Auditorium.java
@@ -1,11 +1,13 @@
 package com.lld.bookmyshow.models;
 
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
 @Getter
 @Setter
+@Entity
 public class Auditorium {
 
     private String name;
--- Low-level-design-folder/bookmyshow/src/main/java/com/lld/bookmyshow/models/BaseModel.java
+++ Low-level-design-folder/bookmyshow/src/main/java/com/lld/bookmyshow/models/BaseModel.java
@@ -1,6 +1,10 @@
 package com.lld.bookmyshow.models;
 
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.Id;
+@MappedSuperclass
 public class BaseModel {
+    @Id
     private String id;
     private String createdAt;
     private String updatedAt;
